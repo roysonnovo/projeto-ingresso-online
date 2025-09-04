50ed70bc0d5c162917c3203d17c894b759f676c7
roysonnovo <royson.novo@gmail.com>
2025-09-04 09:23:33 -0300

    Número válido

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@ function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
     let qtd = parseInt(document.getElementById('qtd').value);
 
+    if (isNaN(qtd) || qtd <= 0) {
+        alert('Por favor, insira uma quantidade válida.');
+        return;
+    }
+
     if(tipo.value == 'pista') {
         comprarPista(qtd);
     } else if(tipo.value == 'superior') {
