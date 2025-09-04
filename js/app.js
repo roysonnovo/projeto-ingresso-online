3bbe551c682cf3825e9f5d7991ddbf4cc9c41494
roysonnovo <royson.novo@gmail.com>
2025-09-04 09:08:49 -0300

    Comprar pista

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
-    let qtd = document.getElementById('qtd');
+    let qtd = parseInt(document.getElementById('qtd').value);
 
+    if(tipo.value == 'pista') {
+        comprarPista(qtd);
+    } 
 }
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if(qtd > qtdPista) {
+        alert('Quantidade indisponnível pata tipo pista');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso');
+    }
+}
+
